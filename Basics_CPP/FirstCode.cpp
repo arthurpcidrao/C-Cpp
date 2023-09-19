62d90578e4feaa1174e69b8b6f367787d75eb417
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-09-19 20:29:22 -0300

    .

--- Basics_CPP/FirstCode.cpp
+++ Basics_CPP/FirstCode.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-using namespace std;
+//using namespace std;
 
 int main(){
 
     int a,b, soma;
 
-    cout << "Digite 2 numeros: ";  // caso não seja definido antes da main o "using namespace std;", escreva: "std::cout << ..."
+    std::cout << "Digite 2 numeros: ";  // caso não seja definido antes da main o "using namespace std;", escreva: "std::cout << ..."
 
-    cin >> a;
-    cin >> b;
+    std::cin >> a;
+    std::cin >> b;
 
     soma = a + b;
 
-    cout << "/nA soma dos numeros é: " << soma << endl;
+    std::cout << "\nA soma dos numeros eh: " << soma << std::endl;
 }
